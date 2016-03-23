863d841ca057b562c5eb7f405e50c4a851f1e840
DavidJBrockway <twitter@pocketserver.co.uk>
2016-03-23 21:25:24 +0000

    Added files via upload

---
+++ game/tntrun/api/MGLobbyType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace game\api;
+
+/**
+ * Lobbyt Type
+ * TntRun PlugIn - MCPE Mini-Game
+ *
+ * Copyright (C) MCPE_PluginDev
+ *
+ * @author DavidJBrockway aka MCPE_PluginDev
+ *        
+ */
+class MGLobbyType {
+	public $STATUS;
+	public $PLAYERS;
+	public $values = [ ];
+	public $lobbyType;
+	public function fromString($s) {
+		return $lobbyType;
+	}
+}
